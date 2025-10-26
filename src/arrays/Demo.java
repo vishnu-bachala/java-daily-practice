58a3ce52b05694ee03708f1e40920e85f8cadb18
vishnu-bachala <vishnuvardhanbachala.gmail.com>
2025-10-26 11:10:46 +0530

    Initial commit - added Java daily practice project

---
+++ src/arrays/Demo.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class Demo {
+    public static void main(String []args){
+        int [] nums = {1,2,3,4,5,6};
+        for(int n : nums){
+            System.out.println(n);
+        }
+    }
+}
---
+++ src/basic/HelloJava.java
@@ -0,0 +1,8 @@
+package basic;
+
+// first hello program
+public class HelloJava {
+    public static void main(String [] args){
+        System.out.println("Hello Java!");
+    }
+}
